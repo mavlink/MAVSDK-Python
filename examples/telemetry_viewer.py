638b6cfdbce220ae0b18f340be8117b82cb36bf9
Matthias Riegler <riegler.matthias2@gmail.com>
2018-05-31 19:32:19 +0200

    Added basic example of how to use the library

---
+++ examples/telemetry_viewer.py
@@ -0,0 +1,37 @@
+"""
+As basic as it can get - this script just dumps some telemetry streams to the
+command line!
+"""
+import sys
+from dronecore import connect
+
+
+if __name__ == "__main__":
+
+    # Connect to a local running backend
+    test = connect("127.0.0.1")
+
+    # Armed stream
+    test.telemetry.armed \
+                  .subscribe(on_next=lambda armed: print("ARMED: ", armed))
+
+    # In air stream
+    test.telemetry.in_air \
+                  .subscribe(on_next=lambda in_air: print("IN_AIR: ", in_air))
+
+    # Position stream
+    test.telemetry.position \
+                  .subscribe(on_next=lambda position: print(position))
+
+    # Health stream
+    test.telemetry.health \
+                  .subscribe(on_next=lambda health: print(health))
+
+    # Battery stream
+    test.telemetry.battery \
+                  .subscribe(on_next=lambda battery: print(battery))
+
+    # We need to block the main thread as the reactive scheduler is running
+    # in the background, gracefull shutdown of the scheduler still has to be
+    # implemented
+    input("Enter to exit\n")
