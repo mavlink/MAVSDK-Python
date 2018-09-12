359ab10e4e7e1aa1b88f8cf8623b8ce50c267905
Matthias Riegler <riegler.matthias2@gmail.com>
2018-09-12 02:26:30 +0200

    Scripted plugin generation, untested

--- other/tools/run_protoc.sh
+++ other/tools/run_protoc.sh
@@ -3,16 +3,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORK_DIR="${SCRIPT_DIR}/../.."
 PROTO_DIR="${WORK_DIR}/proto/"
 GENERATED_DIR="${WORK_DIR}/dronecode_sdk/generated"
+PLUGIN_DIR="${WORK_DIR}/dronecode_sdk/plugins"
 
 function generate {
     for PROTO_FILE in `find ${PROTO_DIR} -name "*.proto" -type f`; do
 
         # Generate bindings for each file individually
-        python -m grpc_tools.protoc -I${GENERATED_DIR} \
-                                    --proto_path=$(dirname ${PROTO_FILE}) \
-                                    --python_out=${GENERATED_DIR} \
-                                    --grpc_python_out=${GENERATED_DIR} \
-                                    ${PROTO_FILE}
+        protoc -I${GENERATED_DIR} \
+               --proto_path=$(dirname ${PROTO_FILE}) \
+               --python_out=${GENERATED_DIR} \
+               --grpc_python_out=${GENERATED_DIR} \
+               ${PROTO_FILE}
 
         # For some reason the import is broken with Python3.5.x and works fine
         # with Python3.6.x, set an absolute path and everything is fine
@@ -22,7 +23,21 @@ function generate {
 
         echo " -> [+] Generated protobuf and gRPC bindings for ${PROTO_IMPORT_NAME%_*}"
 
+        
+        # Generate plugin
+        protoc --plugin=protoc-gen-custom=$(which dcsdkgen) \                        
+            -I$(dirname ${PROTO_FILE}) \
+            --custom_out=${PLUGIN_DIR} \
+            --custom_opt=py \
+            ${PROTO_FILE}
+        echo " -> [+] Generated plugin for ${PROTO_IMPORT_NAME%_*}"
+
     done
+}
+
+function install_dcsdkgen {
+    cd ${PROTO_DIR}/pb_plugins
+    pip install -e .
 }
 
 echo "[+] Updating submodule"
@@ -30,6 +45,9 @@ cd $WORK_DIR
 git submodule init
 git submodule update --recursive
 echo "[+] Done"
-echo "[+] Generating protobuf and gRPC bindings"
+echo "[+] Installing the DronecodeSDK autogenerator"
+install_dcsdkgen
+echo "[+] Done"
+echo "[+] Generating plugins from "
 generate
 echo "[+] Done"
