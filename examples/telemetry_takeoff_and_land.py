572416037529839faafdc34f6d5f45704f93e9d6
Matthias Riegler <riegler.matthias2@gmail.com>
2018-10-18 08:03:11 +0200

    Different approach to create the tasks for the eventloop

--- examples/telemetry_takeoff_and_land.py
+++ examples/telemetry_takeoff_and_land.py
@@ -6,6 +6,7 @@ drone = dronecode_sdk_connect(host="127.0.0.1")
 
 
 async def run():
+    """ Arms the system, initiates a takeof and finally lands the system """
     arm_result = await drone.action.arm()
     print("-- Arm result: " + arm_result[1].action_result.result_str)
     takeoff_result = await drone.action.takeoff()
@@ -18,9 +19,21 @@ async def run():
 
 
 async def print_position():
+    """ Outputs the position """
     async for position in drone.telemetry.position():
         print("Altitude:", position.relative_altitude_m)
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(run(), print_position()))
+def setup_tasks():
+    # Create asyncio tasks with the default eventloop
+    asyncio.create_task(print_position())
+    asyncio.create_task(run())
+
+
+if __name__ == "__main__":
+
+    # Setup asyncio tasks
+    setup_tasks()
+
+    # Runs the event loop until the program is canceled with e.g. CTRL-C
+    asyncio.get_event_loop().run_forever()
