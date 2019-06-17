14f524d3bab7091b8541605c600d6c95f6d4f583
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-06-18 01:40:58 +0200

    add generated sources for windows test

---
+++ mavsdk/plugins/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .offboard import *
+from .calibration import *
+from .gimbal import *
+from .core import *
+from .action import *
+from .param import *
+from .camera import *
+from .info import *
+from .mission import *
+from .telemetry import *
