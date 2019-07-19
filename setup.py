dcc30db1fa527a2cd5f3dba0bd16106737e1d240
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-07-19 09:14:27 +0200

    improve setup.py
    * Read mavsdk_server version from MAVSDK_SERVER_VERSION file
    * Download mavsdk_server as a pre-build step
    * Sets minimum required python to 3.6

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+from distutils.command.build import build
 from setuptools import setup, find_packages
 from codecs import open
 from os import path, getcwd
+
+import urllib.request
+import os
+import stat
+import sys
 
 
 def parse_requirements(filename):
@@ -35,15 +41,72 @@ def parse_long_description():
         return f.read()
 
 
+class custom_build(build):
+    """
+    Class that overrides the build step to add a custom pre-build step
+    """
+
+    @property
+    def platform_suffix(self):
+        """
+        Trying to detect the platform to know which `mavsdk_server` executable to download
+        """
+        if sys.platform.startswith('linux'):
+            return 'manylinux1-x64'
+        else:
+            raise NotImplementedError(
+                f"Platform {sys.platform} is not (yet) supported by this setup.py!")
+
+    @property
+    def mavsdk_server_filepath(self):
+        """
+        The location of the downloaded `mavsdk_server` binary
+        """
+        return 'mavsdk/bin/mavsdk_server'
+
+    @property
+    def mavsdk_server_tag(self):
+        """
+        The release tag of the `mavsdk_server` binary is defined in the file 
+        "MAVSDK_SERVER_VERSION", and used to download the corresponding release
+        """
+        with open("MAVSDK_SERVER_VERSION") as mavsdk_server_version_file:
+            return mavsdk_server_version_file.read().rstrip()
+
+    @property
+    def mavsdk_server_url(self):
+        """
+        Build the url of the `mavsdk_server` binary
+        """
+        return f"https://github.com/mavlink/MAVSDK/releases/download/{self.mavsdk_server_tag}/mavsdk_server_{self.platform_suffix}"
+
+    def run(self):
+        self.download_mavsdk_server()
+        build.run(self)
+
+    def download_mavsdk_server(self):
+        print(
+            f"downloading {self.mavsdk_server_url} into {self.mavsdk_server_filepath}")
+        urllib.request.urlretrieve(
+            self.mavsdk_server_url,
+            filename=self.mavsdk_server_filepath)
+
+        print(f"adding execution permission to {self.mavsdk_server_filepath}")
+        st = os.stat(self.mavsdk_server_filepath)
+        os.chmod(self.mavsdk_server_filepath, st.st_mode | stat.S_IEXEC)
+
+
 setup(
     name="mavsdk",
-    version="0.1.0",
+    version="0.2.0",
     description="Python wrapper for MAVSDK",
     long_description=parse_long_description(),
     url="https://github.com/mavlink/MAVSDK-Python",
     maintainer="Jonas Vautherin, Julian Oes",
     maintainer_email="jonas@auterion.com, julian.oes@auterion.com",
+    python_requires='>=3.6',
     include_package_data=True,
+    cmdclass={'build': custom_build},
 
     classifiers=[
         "Development Status :: 3 - Alpha",
