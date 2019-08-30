7743709de73000aeea14174f408c64df569f96de
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-08-30 09:14:31 +0200

    create a drone class to improve interface and documentation

--- mavsdk/__init__.py
+++ mavsdk/__init__.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import atexit
-import os
 import platform
-import subprocess
 import sys
-import time
 
-from mavsdk import bin
-
-# List of the core plugins
-CORE_PLUGINS = [
-    "Action",
-    "Calibration",
-    "Camera",
-    "Core",
-    "Gimbal",
-    "Info",
-    "Mission",
-    "Param",
-    "Offboard",
-    "Telemetry"
-    ]
+from .drone import Drone
+from .generated import * # NOQA
 
 # Check for compatibility
 if float(".".join(platform.python_version_tuple()[0:-1])) < 3.6:
@@ -38,55 +21,3 @@ try:
 except ImportError:
     # No uvloop installed on the system; the default eventloop works as well!
     pass
-
-
-def get_event_loop():
-    """ Asyncio eventloop """
-    return asyncio.get_event_loop()
-
-
-# Plugins rely on the eventloop
-from .async_plugin_manager import AsyncPluginManager # NOQA
-from .generated import * # NOQA
-
-
-def connect(*args, **kwargs):
-    """
-    Generates a dronecore instance with all available Core plugins registered
-    and ready to use
-    """
-    plugin_manager = AsyncPluginManager(*args, **kwargs)
-
-    for plugin in CORE_PLUGINS:
-        globals()[plugin](plugin_manager)
-
-    return plugin_manager
-
-
-if sys.version_info >= (3, 7):
-    from importlib.resources import path
-else:
-    from importlib_resources import path
-
-
-def start_mavlink(connection_url=None):
-    """
-    Starts the gRPC server in a subprocess, listening on localhost:50051
-    """
-    with path(bin, 'mavsdk_server') as backend:
-        bin_path_and_args = [os.fspath(backend)]
-        if connection_url:
-            bin_path_and_args.append(connection_url)
-        p = subprocess.Popen(bin_path_and_args,
-                             shell=False,
-                             stdout=subprocess.DEVNULL,
-                             stderr=subprocess.DEVNULL)
-
-    # Wait to make sure the gRPC server is started.
-    # There are better, yet more complex ways to do that.
-    time.sleep(2)
-
-    def cleanup():
-        p.kill()
-
-    atexit.register(cleanup)
--- mavsdk/_base.py
+++ mavsdk/_base.py
@@ -6,17 +6,15 @@ class AsyncBase:
     Base implementation for the async gRPC connection
     """
 
-    def __init__(self, async_plugin_manager=None):
-        self.init_plugin(async_plugin_manager)
+    def __init__(self, async_plugin_manager):
+        self._init_plugin(async_plugin_manager)
 
-    def init_plugin(self, async_plugin_manager):
+    def _init_plugin(self, async_plugin_manager):
         """
         Sort of "registers" the plugin to the backend
         """
         if async_plugin_manager:
-            self._loop = async_plugin_manager.loop
             self._setup_stub(async_plugin_manager.channel)
-            async_plugin_manager.register_plugin(self)
 
     def _setup_stub(self, channel):
         """
