5023cd10fbc572be9338f5e5074e14a08b3859dd
Julian Oes <julian@oes.ch>
2019-11-05 16:08:34 +0100

    examples: always wait for drone to connect
    This should get rid of the errors people were seeing just because the
    drone was not ready or had not connected yet.

--- examples/telemetry_flight_mode.py
+++ examples/telemetry_flight_mode.py
@@ -8,6 +8,12 @@ async def print_flight_mode():
     drone = System()
     await drone.connect(system_address="udp://:14540")
 
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+
     async for flight_mode in drone.telemetry.flight_mode():
         print("FlightMode:", flight_mode)
 
