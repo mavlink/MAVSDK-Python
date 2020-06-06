8a573b18c7208442160eadada2f61a997436b3ed
Julian Oes <julian@oes.ch>
2020-06-06 07:52:55 +0200

    sphinx: fix path to create docs

--- mavsdk/source/conf.py
+++ mavsdk/source/conf.py
@@ -4,18 +4,18 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import subprocess
+import sys
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
-import subprocess
 
 project = 'MAVSDK-Python'
 copyright = '2020, Jonas Vautherin, Julian Oes'
