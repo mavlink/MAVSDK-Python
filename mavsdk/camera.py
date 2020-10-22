e82e59cdd2681fc68433f605e5b77225a1943d98
Jonas Vautherin <jonas.vautherin@gmail.com>
2020-10-22 17:57:12 +0200

    Add repeated non-primitive support to streams

--- mavsdk/camera.py
+++ mavsdk/camera.py
@@ -1857,7 +1857,7 @@ class Camera(AsyncBase):
                 
 
             
-                yield [Setting].translate_from_rpc(response.current_settings)
+                yield list(map(lambda x : Setting.translate_from_rpc(x), response.current_settings))
         finally:
             current_settings_stream.cancel()
 
@@ -1881,7 +1881,7 @@ class Camera(AsyncBase):
                 
 
             
-                yield [SettingOptions].translate_from_rpc(response.setting_options)
+                yield list(map(lambda x : SettingOptions.translate_from_rpc(x), response.setting_options))
         finally:
             possible_setting_options_stream.cancel()
 
