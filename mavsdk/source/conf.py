2d279befb242b673339c0023e18fda8ac6cfdce1
Julian Oes <julian@oes.ch>
2020-11-04 12:06:03 +0100

    docs: upload css and add favicon

--- mavsdk/source/conf.py
+++ mavsdk/source/conf.py
@@ -59,3 +59,5 @@ html_theme_path = ["."]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['nature_adapted/static']
+
+html_favicon = "nature_adapted/static/favicon.png"
