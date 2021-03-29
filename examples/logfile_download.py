3cf24643a1d614ecf5ee7e70841212984f535293
Julian Oes <julian@oes.ch>
2021-03-29 12:14:02 +0200

    Add logfile download example

---
+++ examples/logfile_download.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import asyncio
+from mavsdk import System
+import sys
+
+
+async def run():
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered")
+            break
+
+    entries = await get_entries(drone)
+    for entry in entries:
+        await download_log(drone, entry)
+
+
+async def download_log(drone, entry):
+    date_without_colon = entry.date.replace(":", "-")
+    filename = f"./log-{date_without_colon}.ulog"
+    print(f"Downloading: log {entry.id} from {entry.date} to {filename}")
+    previous_progress = -1
+    async for progress in drone.log_files.download_log_file(entry, filename):
+        new_progress = round(progress.progress*100)
+        if new_progress != previous_progress:
+            sys.stdout.write(f"\r{new_progress} %")
+            sys.stdout.flush()
+            previous_progress = new_progress
+    print()
+
+
+async def get_entries(drone):
+    entries = await drone.log_files.get_entries()
+    for entry in entries:
+        print(f"Log {entry.id} from {entry.date}")
+    return entries
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(run())
