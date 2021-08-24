f7804bfed92f97b27f319bceae59d98cc42cf671
Jay Sharma <jaysharma13coolstar@gmail.com>
2021-08-25 02:29:08 +0530

    Commented, made print and log better.

--- examples/Rover.py
+++ examples/Rover.py
@@ -8,103 +8,48 @@ from mavsdk.mission import (MissionItem, MissionPlan)
 from mavsdk.telemetry import (FlightMode)
 
 recList=[] #to record position when in record mode.
-
-async def run():
-    # Init the drone
-    drone = System()
-    await drone.connect(system_address="udp://:14540")
-
-    asyncio.ensure_future(print_battery(drone))
-    asyncio.ensure_future(print_gps_info(drone))
-    asyncio.ensure_future(print_in_air(drone))
-    asyncio.ensure_future(print_position(drone))
-    asyncio.ensure_future(log(drone))
-    asyncio.ensure_future(fly(drone))
-
-async def print_mission_progress(drone):
-    async for mission_progress in drone.mission.mission_progress():
-        print(f"Mission progress: "
-              f"{mission_progress.current}/"
-              f"{mission_progress.total}")
-
-async def observe_is_in_air(drone, running_tasks):  #killer function
-    """ Monitors whether the drone is flying or not and
-    returns after landing """
-
-    diditevertakeoff = False
-
-    async for is_in_air in drone.telemetry.in_air():
-        if is_in_air:
-            diditevertakeoff = is_in_air
-
-        if diditevertakeoff and not is_in_air:
-            for task in running_tasks:
-                task.cancel()
-                try:
-                    await task
-                except asyncio.CancelledError:
-                    pass
-            await asyncio.get_event_loop().shutdown_asyncgens()
-            return
-
-async def fly(drone):   #main function
-    print_mission_progress_task = asyncio.ensure_future(print_mission_progress(drone))
-    running_tasks = [print_mission_progress_task]
-    termination_task = asyncio.ensure_future(
-        observe_is_in_air(drone, running_tasks))
-    await termination_task
-
 #EDITABLE CODE STARTS HERE
 
-async def print_position(drone):
-    async for position in drone.telemetry.position():
-        print(position)
+async def print_Telemetry_All(drone):
+    async for telemetry in drone.telemetry():
+        print(f"Flight Mode: {telemetry.FlightMode}") #printing Flightmode
+        print(f"Position: {telemetry.position}") #Printing position
+        print(f"Battery Left: {telemetry.battery.remaining_percent}") #Printing REMAINING BATTERY 
+        print(f"GPS Info: {telemetry.gps_info}")#Printing GPS info
+        print(f"Mission progress: {drone.mission.mission_progress.current} / {drone.mission.mission_progress.ctotal}")
+        #Printing mission progress
+        await asyncio.sleep(1)
 
-async def print_battery(drone):
-    async for battery in drone.telemetry.battery():
-        print(f"Battery: {battery.remaining_percent}")
-
-
-async def print_gps_info(drone):
-    async for gps_info in drone.telemetry.gps_info():
-        print(f"GPS info: {gps_info}")
-
-
-async def print_in_air(drone):
-    async for in_air in drone.telemetry.in_air():
-        print(f"In air: {in_air}")
-
-async def log(drone):
+async def log(drone): #logging it all in a file.
     with open('output.txt', 'w') as f:
         async for telem in drone.telemetry():
-            f.write(str(telem.position.latitude_deg)+" | "+str(telem.position.longitude_deg)+"\n")
-            f.write("GPS Info:"+str(telem.gps_info)+" | "+str(telem.)"\n")
-            await asyncio.sleep(0.5)
+            f.write(str(f"Flight Mode: {telemetry.FlightMode} | Position: {telem.position} | Battery Left: {telem.battery.remaining_percent} | GPS Info: {telem.gps_info} | Mission progress: {drone.mission.mission_progress.current} / {drone.mission.mission_progress.ctotal}"))
+            await asyncio.sleep(1)
 
-def getMissionPlan(copiedlist):
-    mission_items=[]
-    for itr in copiedlist:
+def getMissionPlan(copiedlist): #make a mission plan from copied recorded list
+    mission_items=[] #temporary mission items list
+    for itr in copiedlist: #filling in all the mission items in the list
         mission_items.append(MissionItem(itr[0],itr[1],25,10,True,float('nan'),float('nan'),
         MissionItem.CameraAction.NONE,float('nan'),float('nan'),float('nan'),float('nan')))
+    return MissionPlan(mission_items) #returning with the mission plan made out of mission items
 
-    return MissionPlan(mission_items)
+async def Record(drone): 
+    global recList 
+    recList.clear() #clear it before starting to record.
+    async for mode in telemetry.FlightMode(): 
+        if mode==9: # Manual mode
+            print("Record mode triggered") 
+            async for pos in drone.telemetry.position(): #append the coordinates to the list. 
+                recList.append((pos.latitude_deg),(pos.longitude_deg))
+                await asyncio.sleep(1)
 
-async def Record(drone):
-    global recList
-    recList.clear()
+async def PB(): #playback mode
     async for mode in telemetry.FlightMode():
-        if mode==9:
-            print("Record mode triggered")
-            async for pos in drone.telemetry.position():
-                recList.append((pos.latitude_deg),(pos.longitude_deg))            
-
-async def PB():
-    async for mode in telemetry.FlightMode():
-        if mode==10:
+        if mode==10: # Altitude Control mode
             print("Playback mode triggered")
             mission_items=[]
             copy_list=recList.copy()
-            mission_Plan=getMissionPlan(copy_list)
+            mission_Plan=getMissionPlan(copy_list) 
 """
     await drone.mission.set_return_to_launch_after_mission(False)
 
@@ -125,6 +70,12 @@ async def PBL(drone):
             copy_list=recList.copy()
             loopList=recList.reverse()+recList
             mission_Plan=getMissionPlan(loopList)
+
+async def run():
+    drone = System() # Init the drone
+    await drone.connect(system_address="udp://:14540") # Connect the drone
+    asyncio.ensure_future(print_Telemetry_All(drone)) #Print all important data
+    asyncio.ensure_future(log(drone)) #Log Position and battery.
 
 #EDITABLE CODE ENDS HERE
 
