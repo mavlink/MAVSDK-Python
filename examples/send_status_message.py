3679e750ae2d89ea585b3cf811bab88da20b5c0a
Dima Danylevskyi <dima.danylevskyi@jet.dev>
2022-04-02 05:13:35 +0300

    Add example for server_utility plugin

---
+++ examples/send_status_message.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import asyncio
+from mavsdk import System
+from mavsdk.server_utility import StatusTextType
+
+"""
+This example shows how to use server_utility plugin to send status messages.
+
+The messages will appear in GCS log. In order to debug messages you can use:
+  - QGroundControll MAVLink Inspector
+  - Wireshark (https://mavlink.io/en/guide/wireshark.html)
+
+In this example we are changing sysid in order to show our message along the
+other messages in QGroundControll interface.
+"""
+
+
+async def run():
+
+    drone = System(sysid=1)
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            await drone.server_utility.send_status_text(StatusTextType.INFO, "Hello world!")
+            break
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
