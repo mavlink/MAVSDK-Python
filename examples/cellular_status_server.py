f8ff4463d3086fc5747741471934bd4e433cc8b3
Mohamad Akkawi <akkawi@protonmail.com>
2023-02-14 00:58:24 -0500

    update cellular example script

--- examples/cellular_status_server.py
+++ examples/cellular_status_server.py
@@ -4,6 +4,8 @@ from mavsdk import System
 from mavsdk.telemetry_server import CellularStatus
 from mavsdk.telemetry_server import TelemetryServerError
 from mavsdk.telemetry_server import ModemInfo
+from mavsdk.telemetry_server import OnboardComputerStatus
+from mavsdk.telemetry_server import ComponentInfoBasic
 
 import asyncio
 
@@ -30,7 +32,7 @@ async def run():
 
         await asyncio.sleep(1)
 
-        modem_info = ModemInfo(1,2,3,4,"id","firmware","model")
+        modem_info = ModemInfo(1,2,3,"modem_id","iccid","firmware","model")
         print(modem_info)
         if await drone.telemetry_server.publish_modem_info(modem_info) is not TelemetryServerError:
             print("OK")
@@ -52,7 +54,7 @@ async def run():
 
         await asyncio.sleep(1)
 
-        modem_info = ModemInfo(2,2,3,4,"id","firmware","model")
+        modem_info = ModemInfo(2,2,3,"modem_id","iccid","firmware","model")
         print(modem_info)
         if await drone.telemetry_server.publish_modem_info(modem_info) is not TelemetryServerError:
             print("OK")
@@ -62,6 +64,33 @@ async def run():
         print("pub modem_info instance 2")
 
         await asyncio.sleep(1)
+
+        comp_info = ComponentInfoBasic(1,2,"vendor_name","modem_name","software_version","hardware_version","serial_number")
+        print(comp_info)
+        if await drone.telemetry_server.publish_component_info_basic(comp_info) is not TelemetryServerError:
+            print("OK")
+        else:
+            print("mavsdk ran into error")
+            return "error :("
+        print("pub comp_info")
+
+
+        await asyncio.sleep(1)
+
+        a = [1,1,1]
+
+        ocs_info = OnboardComputerStatus(1,2,3,a,a,a,a,4,a,a,5,6,a,a,a,a,a,a,a,a)
+
+        print(ocs_info)
+        if await drone.telemetry_server.publish_onboard_computer_status(ocs_info) is not TelemetryServerError:
+            print("OK")
+        else:
+            print("mavsdk ran into error")
+            return "error :("
+        print("pub ocs_info")
+
+        await asyncio.sleep(1)
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
