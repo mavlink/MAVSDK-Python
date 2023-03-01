dc86909d8dc155bc5ceb0fa526ee92330ac25d42
Julian Oes <julian@oes.ch>
2023-03-02 10:49:28 +1300

    upload_params: couple of taste changes
    I changed a few things according to how I'd do it:
    - Don't use time.sleep in async.
    - Use early return or continue to avoid nesting too deep.
    - Use the usual MAVSDK connection string instead of type and port.
    - Use new asyncio syntax (no more getting the event loop).
    - Avoid opening file twice.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

--- examples/upload_params.py
+++ examples/upload_params.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import asyncio
-import time
 import argparse
 from mavsdk import System
 from tqdm import tqdm
@@ -10,67 +9,56 @@ from tqdm import tqdm
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "param_file", help="Param file to be uploaded with .params format")
+        "connection",
+        help="Connection string (e.g. udp://:14540)")
     parser.add_argument(
-        "connection_type", help="The connection type, either 'udp' or 'serial' python3 upload_params.py serial /dev/tty.usbmodem01:115200 or python3 upload_params.py udp :14540")
-    parser.add_argument("port", help="The port for the connection")
+        "param_file", help="Param file to be uploaded with .params format")
 
     args = parser.parse_args()
-    system_address = f"{args.connection_type}://{args.port}"
-    param_file_name = f"{args.param_file}"
 
-    with open(param_file_name) as file:
-        for line in file:
-            if not line.startswith("#"):
-                columns = line.strip().split("\t")
-                vehicle_id = columns[0]
-                component_id = columns[1]
-                name = columns[2]
-                value = columns[3]
-                type = columns[4]
+    asyncio.run(set_params())
 
-    async def set_params():
-        drone = System()
-        await drone.connect(system_address=system_address)
-        print("Connected to the Vehicle")
-        param_plugin = drone.param
-        params = await param_plugin.get_all_params()
-        float_params = params.float_params
-        int_params = params.int_params
-        custom_params = params.custom_params
-        int_param_names = [p.name for p in int_params]
-        float_param_names = [p.name for p in float_params]
-        custom_param_names = [p.name for p in custom_params]
 
-        while True:
-            async for is_in_air in drone.telemetry.in_air():
-                if is_in_air:
-                    print("The Vehicle not landed!")
-                    time.sleep(4)
-                else:
-                    break
+async def set_params():
+    drone = System()
+    await drone.connect(system_address=args.connection)
+    print("Connected to the Vehicle")
+    param_plugin = drone.param
+    params = await param_plugin.get_all_params()
+    float_params = params.float_params
+    int_params = params.int_params
+    custom_params = params.custom_params
+    int_param_names = [p.name for p in int_params]
+    float_param_names = [p.name for p in float_params]
+    custom_param_names = [p.name for p in custom_params]
 
-            with open(param_file_name, "r") as param_file:
-                print("Uploading Parameters! Please do not arm the vehicle!")
-                for line in tqdm(param_file, unit='lines'):
-                    if not line.startswith("#"):
-                        columns = line.strip().split("\t")
-                        vehicle_id = columns[0]
-                        component_id = columns[1]
-                        name = columns[2]
-                        value = columns[3]
-                        type = columns[4]
-                        if name in int_param_names:
-                            await drone.param.set_param_int(name, int(value))
-                        elif name in float_param_names:
-                            await drone.param.set_param_float(name, float(value))
-                        elif name in custom_param_names:
-                            await drone.param.set_param_custom(name, value)
+    while True:
+        async for is_in_air in drone.telemetry.in_air():
+            if is_in_air:
+                print("Waiting until vehicle is landed...")
+            else:
+                break
 
-            print("Params uploaded!")
+    with open(args.param_file, "r") as param_file:
+        print("Uploading Parameters... Please do not arm the vehicle!")
+        for line in tqdm(param_file, unit='lines'):
+            if line.startswith("#"):
+                continue
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(set_params())
+            columns = line.strip().split("\t")
+            vehicle_id = columns[0]
+            component_id = columns[1]
+            name = columns[2]
+            value = columns[3]
+            type = columns[4]
+            if name in int_param_names:
+                await drone.param.set_param_int(name, int(value))
+            elif name in float_param_names:
+                await drone.param.set_param_float(name, float(value))
+            elif name in custom_param_names:
+                await drone.param.set_param_custom(name, value)
+
+    print("Params uploaded!")
 
 
 if __name__ == "__main__":
