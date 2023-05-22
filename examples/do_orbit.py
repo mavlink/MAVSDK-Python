e5e8c6bf04e6d3af597bd64e2495540f9c9539a8
arpit.p <rakeshshastri26@gmail.com>
2023-05-22 13:02:38 +0530

    Updated code after checking code style

--- examples/do_orbit.py
+++ examples/do_orbit.py
@@ -2,6 +2,7 @@ import asyncio
 from mavsdk import System
 from mavsdk.action import OrbitYawBehavior
 
+
 async def run():
     drone = System()
     await drone.connect(system_address="udp://:14540")
@@ -17,9 +18,10 @@ async def run():
         if health.is_global_position_ok and health.is_home_position_ok:
             print("-- Global position estimate OK")
             break
-    
+
     position = await drone.telemetry.position().__aiter__().__anext__()
     orbit_height = position.absolute_altitude_m+10
+    yaw_behavior = OrbitYawBehavior.HOLD_FRONT_TO_CIRCLE_CENTER
 
     print("-- Arming")
     await drone.action.arm()
@@ -28,13 +30,13 @@ async def run():
     await drone.action.takeoff()
     await asyncio.sleep(10)
 
-    print('Do orbit at 10m height from the ground')    
-    await drone.action.do_orbit(radius_m = 10,
-                                velocity_ms = 2,
-                                yaw_behavior = OrbitYawBehavior.HOLD_FRONT_TO_CIRCLE_CENTER,                                
-                                latitude_deg = 47.398036222362471,
-                                longitude_deg = 8.5450146439425509,
-                                absolute_altitude_m = orbit_height)
+    print('Do orbit at 10m height from the ground')
+    await drone.action.do_orbit(radius_m=10,
+                                velocity_ms=2,
+                                yaw_behavior=yaw_behavior,
+                                latitude_deg=47.398036222362471,
+                                longitude_deg=8.5450146439425509,
+                                absolute_altitude_m=orbit_height)
     await asyncio.sleep(60)
 
     await drone.action.return_to_launch()
