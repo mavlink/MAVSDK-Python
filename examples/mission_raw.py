c6e7ed04d2c07a855c4517f330c87f41e8f482cc
Abdur-Rahman <abbadshaf05@gmail.com>
2024-05-14 08:06:09 +0100

    Update mission_raw.py
    Updated [MAV_FRAME](https://mavlink.io/en/messages/common.html#MAV_FRAME) command from 6 (deprecated) to 3 (newly updated WGS84 + rel. system)
    
    Also added a few comments to make comprehension easier
    
    Refactored file

--- examples/mission_raw.py
+++ examples/mission_raw.py
@@ -22,29 +22,34 @@ async def run():
 
 
 async def run_drone(drone):
-
     mission_items = []
 
     mission_items.append(mission_raw.MissionItem(
-         0,  # start seq at 0
-         6,
-         16,
-         1,  # first one is current
-         1,
-         0, 10, 0, float('nan'),
-         int(47.40271757 * 10**7),
-         int(8.54285027 * 10**7),
-         30.0,
-         0
+         0,                         # start seq at 0
+         3,                         # MAV_FRAME command. 3 is WGS84 + relative altitude
+         16,                        # command. 16 is a basic waypoint
+         1,                         # first one is current
+         1,                         # auto-continue. 1: True, 0: False
+         0,                         # param1 - hold time
+         10,                        # param2 - Acceptance radius (if the sphere with this radius is hit, the waypoint counts as reached)
+         0,                         # param3 - 0 means it should pass through the waypoint normally
+         float('nan'),              # param4 - Desired yaw angle at waypoint. NaN to use the current system yaw heading mode
+         int(47.40271757 * 10**7),  # param5 - latitude (multiplying by 10^7 due to MAV_FRAME)
+         int(8.54285027 * 10**7),   # param6 - longitude
+         30.0,                      # param7 - altitude
+         0                          # mission_type. Specifies this item as a main command for the mission
      ))
 
     mission_items.append(mission_raw.MissionItem(
         1,
-        6,
+        3,
         16,
         0,
         1,
-        0, 10, 0, float('nan'),
+        0,
+        10,
+        0,
+        float('nan'),
         int(47.40271757 * 10**7),
         int(8.54361892 * 10**7),
         30.0,
